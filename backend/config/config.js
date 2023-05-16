e961689563a1ef3427d81fc8f6c2853c4668f300
StreamLV <l.vitaliy.m@gmail.com>
2023-05-16 19:02:48 +0300

    feat (ole 32)

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,8 +1,21 @@
 module.exports = {
   app: {
-    port: 5712,
+    port: 5715,
     dummyFr: false,
     dummyFrError: false,
     token: 'a3cbe070577cf13367904316f5d3f037',
+  },
+  fr: {
+    workDirectory: 'F:/Bases/dev_webCashBox/exellioOle2http/backend/builds/build-win64/frCategory',
+    timeout: 5000,
+    type: 'serial',
+    serialConfig: {
+      port: 'com1',
+      speed: 115200
+    },
+    httpConfig: {
+      ip: '',
+      port: ''
+    }
   }
 };
